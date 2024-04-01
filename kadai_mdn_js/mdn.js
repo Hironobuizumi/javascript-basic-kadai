59755838af619b01a82c0b511167e28bb6075b45
Hironobuizumi <158254076+Hironobuizumi@users.noreply.github.com>
2024-04-01 15:02:36 +0900

    kadaimdn

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+var date = new Date();         
+var yyyy = date.getFullYear(); 
+var mm = date.getMonth() + 1;  
+var dd = date.getDate(); 
+var w = date.getDay();   
+ 
+  
+var result = yyyy + "年" + mm + "月" + dd + "日" ; 
+ 
+console.log(result);
